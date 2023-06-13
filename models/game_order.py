739fb36dc66de7a0cbbcc0f6a236da39f6ba1d6e
Nadi Campbell <nadicampbell97@hotmail.com>
2023-06-13 20:43:29 +0100

    homework wk3 day2

---
+++ models/game_order.py
@@ -0,0 +1,9 @@
+from models.order import *
+
+
+order1 = Order("John", "Diablo IV", "22 Jun", 1)
+order2 = Order("Sean", "Red Dead Redemption", "14 Jun", 4)
+order3 = Order("Jarrod", "Call Of Duty mw2", "26 July", 2)
+order4 = Order("Stan", "The Last Of Us", "17 July", 2)
+
+orders = [order1, order2, order3, order4]
